c211d4445ad114527ba1ee767f4f71c43c6113e5
d4niel <olayajuandaniel8@gmail.com>
2022-05-07 17:08:31 -0500

    Filtro buscar por cedula

--- consultaempleados.php
+++ consultaempleados.php
@@ -1,4 +1,26 @@
 <?php
     include('conexion.php');
-    
+    if(isset($_GET['buscarEmp'])){
+        $busqueda_cedula = $_GET['buscarEmp'];
+        $busqueda_cedula = trim($busqueda_cedula);
+        $consulta_empleado = "SELECT nombre FROM empleados WHERE cedula = '$busqueda_cedula' ";
+        function buscarCedula($conexion, $consulta){
+        if($busqueda_emp = mysqli_prepare($conexion, $consulta)){
+            mysqli_stmt_execute($busqueda_emp);
+            mysqli_stmt_bind_result($busqueda_emp, $cedula);
+            $indicador_cedula = mysqli_stmt_fetch($busqueda_emp);
+            return $indicador_cedula;
+            var_dump($indicador_cedula);
+            
+        }
+        }
+        $busqueda = 'encontrada';
+        if(!buscarCedula($conexion, $consulta_empleado)){
+            $busqueda = 'no_encontrada';
+
+        }
+        header('Location:empleados.php?busqueda='.$busqueda.'&cedula='.$busqueda_cedula);
+
+        
+    }
 ?>
--- empleados.php
+++ empleados.php
@@ -3,9 +3,8 @@
 // Traigo las variables del archivo conexion.php
     include('conexion.php');
     // consulta que quiero mostrar en la página
-    $mostrar = "SELECT empleados.cedula, empleados.nombre, empleados.dias_lab, devengados.sueldo, devengados.valor_horas_total,
-    devengados.auxilio_transporte, devengados.total_dev, deducciones.salud, deducciones.otras_deduc as 'Prestamos',
-deducciones.total_deducido FROM empleados INNER JOIN devengados INNER JOIN deducciones ON empleados.id = devengados.id_empleado AND empleados.id = deducciones.id_devengado;
+    $mostrar = "SELECT empleados.cedula, empleados.nombre, empleados.apellido, departamentos.nombre as 'Departamento',
+     cargos.nombre as 'Cargo' FROM empleados INNER JOIN departamentos INNER JOIN cargos ON empleados.id_departamento = departamentos.id AND empleados.id_cargo = cargos.id;
 ";
 
 ?>
@@ -26,21 +25,39 @@ deducciones.total_deducido FROM empleados INNER JOIN devengados INNER JOIN deduc
             <label for="busquedaEmp">Buscar empleado por la cedula</label>
             <input type="search" id="busquedaEmp" name="buscarEmp">
             <button>Buscar</button>
-        </form> 
+        </form>
+        <select name="opciones" id="opciones">
+            <optgroup label="Fitros">
+                
+                <option value="cargo">Cargo</option>
+                <option value="departamentos">Departamentos</option>
+            </optgroup>
+        </select>
+        <?php
+            if(isset($_GET['busqueda'])){
+                $estado_cedula = $_GET['busqueda'];
+                if($estado_cedula == 'encontrada'){
+                    $cedula = $_GET['cedula'];
+                    echo '<h2>Cedula encontrada</h2>';
+                    $mostrar = $mostrar = "SELECT empleados.cedula, empleados.nombre, empleados.apellido, departamentos.nombre as 'Departamento',
+                    cargos.nombre as 'Cargo' FROM empleados INNER JOIN departamentos INNER JOIN cargos ON empleados.id_departamento = departamentos.id AND empleados.id_cargo = cargos.id
+                    WHERE cedula = '$cedula';";
+                }
+                else{
+                    echo '<h2>Cedula no encontrada</h2>';
+                }
+            }
+        ?>
     </div>
     <br>   
     <table border='1' id="empleados">
         <tr>
             <th>Cedula</th>
             <th>Nombre</th>
-            <th>Dias laborados</th>
-            <th>Sueldo</th>
-            <th>Valor horas extras</th>
-            <th>Auxilio de transporte</th>
-            <th>Total devengados</th>
-            <th>Salud</th>
-            <th>Prestamos</th>
-            <th>Total deducido</th>
+            <th>Apellido</th>
+            <th>Departamento</th>
+            <th>Cargo</th>
+            
         </tr>
         <?php
         $tabla = mysqli_query($conexion, $mostrar);
@@ -48,14 +65,10 @@ deducciones.total_deducido FROM empleados INNER JOIN devengados INNER JOIN deduc
         <tr>
             <td><?php echo $row['cedula'];?></td>
             <td><?php echo $row['nombre'];?></td>
-            <td><?php echo $row['dias_lab'];?></td>
-            <td><?php echo $row['sueldo'];?></td>
-            <td><?php echo $row['valor_horas_total'];?></td>
-            <td><?php echo $row['auxilio_transporte'];?></td>
-            <td><?php echo $row['total_dev'];?></td>
-            <td><?php echo $row['salud'];?></td>
-            <td><?php echo $row['Prestamos'];?></td>
-            <td><?php echo $row['total_deducido'];?></td>
+            <td><?php echo $row['apellido'];?></td>
+            <td><?php echo $row['Departamento'];?></td>
+            <td><?php echo $row['Cargo'];?></td>
+           
         </tr>
         <?php } mysqli_free_result($tabla); ?>
     </table>
